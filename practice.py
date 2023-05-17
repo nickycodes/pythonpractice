4f16a374a9efd3bf514f1500b14e5ef2b366f731
Nicholas Coamey <84030698+nickycodes@users.noreply.github.com>
2023-05-17 08:46:32 -0500

    Add files via upload

--- practice.py
+++ practice.py
@@ -1,5 +1,14 @@
-first_string = "Test"
-second_string = "Message"
+# first_string = "Test"
+# second_string = "Message"
 
-message = "{} {}".format(first_string, second_string)
-print(message)
+# message = "{} {}".format(first_string, second_string)
+# print(message)
+
+age = 30
+
+earnings = 785.82
+
+name = "nickyC"
+
+print("Hello my name is " + str(name) + " and my age is " +
+      str(age) + " and I have " + str(earnings) + " in my account")
